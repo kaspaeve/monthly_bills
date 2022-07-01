628e384a4a67cbde53bf43efb00424b35b8842ee
kaspaeve <koga.industries@gmail.com>
2022-07-01 18:38:56 -0500

    corrected loop for sql

--- bills_add_monthly_submit.php
+++ bills_add_monthly_submit.php
@@ -7,8 +7,10 @@ if(isset($_POST['save_multi']))
 
 
     $bill_list = $_POST['bill_list'];
+    $bill_list2 = $_POST['lstBox2'];
     $year = ($_POST['year']);
     $month = ($_POST['month']);
+
 
     foreach($bill_list as $bills)
     {
@@ -26,15 +28,15 @@ if(isset($_POST['save_multi']))
       $bill_paid_amount  = $row['bill_paid_amount'];
       $bill_notes  = $row['bill_notes'];
 
-    //     echo $bills."<br>";
-  //       echo $year."<br>";
-    //     echo $month."<br>";
-    //     echo $bill_account. "<br>";
-    //     echo $bill_website. "<br>";
-    //     echo $bill_notes. "<br>";
-  //       echo $bill_due_date. "<br>";
-  //       echo $bill_user. "<br>";
-  //       echo "<br>";
+//         echo $bills."<br>";
+//         echo $year."<br>";
+//         echo $month."<br>";
+//        echo $bill_account. "<br>";
+//         echo $bill_website. "<br>";
+//         echo $bill_notes. "<br>";
+//         echo $bill_due_date. "<br>";
+//         echo $bill_user. "<br>";
+//         echo "<br>";
 
 
 
@@ -42,25 +44,38 @@ if(isset($_POST['save_multi']))
             $result3 = $conn->query($sql3);
             $row3 = $result3->fetch_row();
 
-            if($row3[0]) {
-              $_SESSION['success_message'] = "Failed to save monthly report.";
-              header("Location: pages/bills_add_monthly.php");
-                $conn->close();
+             if($row3[0]) {
+               $_SESSION['success_message'] = "Failed to save monthly report.";
+               header("Location: pages/bills_add_monthly.php");
+                 $conn->close();
+
             } else {
               mysqli_query($conn, "INSERT INTO history(id_bill, bill_name, bill_date, bill_paid_date, bill_paid_amount, bill_notes, Year, Month) VALUES ('$bill_id','$bill_name','$bill_due_date','$bill_paid_date','$bill_paid_amount','$bill_notes','$year', '$month')");
 
-              //Start the session if already not started.
-              session_start();
-              $_SESSION['success_message'] = "New monthly report saved successfully.";
-              header("Location: pages/bills_add_monthly.php");
-              exit();
+      //        echo $bills."<br>";
+      //        echo $year."<br>";
+      //        echo $month."<br>";
+      //       echo $bill_account. "<br>";
+      //        echo $bill_website. "<br>";
+      //        echo $bill_notes. "<br>";
+      //        echo $bill_due_date. "<br>";
+      //        echo $bill_user. "<br>";
+      //        echo "<br>";
+
+
+
               }
+              //Start the session if already not started.
 
 
 }
 
-    }
 
+    }
+    session_start();
+    $_SESSION['success_message'] = "New monthly report saved successfully.";
+    header("Location: pages/bills_add_monthly.php");
+    exit();
 
 }
 ?>
