7b1db6dbe8fa0bac04778ed9cd26b191975b3f38
kaspaeve <koga.industries@gmail.com>
2022-07-10 14:45:19 -0500

    init

---
+++ include/footer.php
@@ -0,0 +1,24 @@
+<footer class="footer pt-5 mt-5">
+  <div class="container">
+    <div class=" row">
+      <div class="col-md-3 mb-4 ms-auto">
+        <div>
+
+      <div class="col-12">
+        <div class="text-center">
+          <p class="text-dark my-4 text-sm font-weight-normal">
+            All rights reserved. Copyright © <script>
+              document.write(new Date().getFullYear())
+            </script> made with <i class="fa fa-heart"></i> by
+            <a href="https://schellschockmedia.com" class="font-weight-bold" target="_blank">SchellSchock Media</a>
+            for a better web.
+
+          </p>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+</div>
+
+</footer>
