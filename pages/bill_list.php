915e80b44037df30f5892a62fa713217a612bbd0
kaspaeve <koga.industries@gmail.com>
2022-07-12 18:03:13 -0500

    corrected alert

--- pages/bill_list.php
+++ pages/bill_list.php
@@ -7,13 +7,9 @@ if(!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true){
     header("location: ../login.php");
     exit;
 }
-
-
 ?>
-
 <!doctype html>
 <html lang="en">
-
 <head>
   <title>Monthly Bill</title>
   <!-- Required meta tags -->
@@ -38,13 +34,10 @@ if(!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true){
   <script src="../assets/js/dataTables.bootstrap4.min.js"></script>
 <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
 </head>
-
 <body>
   <!--  Navbar -->
   <?php include_once '../include/navbar.php'; ?>
-
   <!-- End Navbar -->
-
   <div class="page-header min-height-400" style="background-image: url(&#39;https://images.unsplash.com/photo-1520769945061-0a448c463865?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1950&q=80&#39;);" loading="lazy">
     <span class="mask bg-gradient-dark opacity-8"></span>
     <div class="container">
@@ -52,7 +45,6 @@ if(!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true){
       </div>
     </div>
   </div>
-
   <div class="card card-body shadow-xl mx-3 mx-md-4 mt-n6">
     <!--bread-->
     <div class="container py-6 mt-2">
@@ -64,20 +56,15 @@ if(!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true){
           </ol>
           <h6 class="font-weight-bolder mb-0">List Bills</h6>
         </nav>
-
       </div>
     </div>
     <!--end bread-->
-
     <div class="container-fluid py-4">
       <!--testing-->
       <div class="table-responsive-xl">
         <table id="sortTable" class="table table-striped table-hover" style="width:100%">
-
           <thead>
-
             <tr>
-
               <th></th>
               <th>Bill Name</th>
               <th>Bill Account #</th>
@@ -95,17 +82,12 @@ if(!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true){
               </th>
               <th>Id</th>
             </tr>
-
           </thead>
-
-          <tbody><tr>
+        <tbody><tr>
             <?php
-
             include_once '../config_save.php';
-
             //      $result = mysqli_query($conn,"SELECT * FROM default_bills");
             $sql = "SELECT id_bill, bill_name, bill_due_date, bill_website, bill_account, bill_user, bill_notes, bill_paid_date, bill_paid_amount FROM default_bills";
-
             $result = $conn->query($sql);
             if ($result->num_rows > 0) {
               // output data of each row
@@ -165,7 +147,7 @@ if(!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true){
                                 <div class="col-lg-4">
                                   <div class="input-group input-group-static mb-4">
                                     <label for="bdate">Due Date</label>
-                                    <select class="form-control" id="bdate" name="bdate">
+                                    <select class="form-control " id="bdate" name="bdate">
                                       <option value selected = "<?php echo $row['bill_due_date'];?>"><?php echo $row['bill_due_date'];?></option>
                                       <option value = "1st">1st</option>
                                       <option value = "2nd">2nd</option>
@@ -224,7 +206,6 @@ if(!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true){
                             <input type="submit" class="btn btn-danger w-auto me-2" name="delete_bill" value="Delete"/>
                             <button type="button" class="btn bg-gradient-dark  w-auto me-2" data-bs-dismiss="modal">Close</button>
                             <input type="submit" class="btn bg-gradient-primary-custom w-auto me-2" name="updatebill" value="Save changes"/>
-
                           </div>
                         </div>
                       </div>
@@ -243,53 +224,9 @@ if(!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true){
           ?>
         </tbody>
       </table>
-
-
-
-
-    <hr class="bg-primary border-2 border-top border-primary">
-    <?php if (isset($_SESSION['update_success_message']) && !empty($_SESSION['update_success_message'])) { ?>
-      <svg xmlns="http://www.w3.org/2000/svg" style="display: none;">
-        <symbol id="check-circle-fill" fill="currentColor" viewBox="0 0 16 16">
-          <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zm-3.97-3.03a.75.75 0 0 0-1.08.022L7.477 9.417 5.384 7.323a.75.75 0 0 0-1.06 1.06L6.97 11.03a.75.75 0 0 0 1.079-.02l3.992-4.99a.75.75 0 0 0-.01-1.05z"/>
-        </symbol>
-        <symbol id="info-fill" fill="currentColor" viewBox="0 0 16 16">
-          <path d="M8 16A8 8 0 1 0 8 0a8 8 0 0 0 0 16zm.93-9.412-1 4.705c-.07.34.029.533.304.533.194 0 .487-.07.686-.246l-.088.416c-.287.346-.92.598-1.465.598-.703 0-1.002-.422-.808-1.319l.738-3.468c.064-.293.006-.399-.287-.47l-.451-.081.082-.381 2.29-.287zM8 5.5a1 1 0 1 1 0-2 1 1 0 0 1 0 2z"/>
-        </symbol>
-        <symbol id="exclamation-triangle-fill" fill="currentColor" viewBox="0 0 16 16">
-          <path d="M8.982 1.566a1.13 1.13 0 0 0-1.96 0L.165 13.233c-.457.778.091 1.767.98 1.767h13.713c.889 0 1.438-.99.98-1.767L8.982 1.566zM8 5c.535 0 .954.462.9.995l-.35 3.507a.552.552 0 0 1-1.1 0L7.1 5.995A.905.905 0 0 1 8 5zm.002 6a1 1 0 1 1 0 2 1 1 0 0 1 0-2z"/>
-        </symbol>
-      </svg>
-      <div class="alert alert-success text-white alert-dismissible d-flex align-items-center fade show" role="alert">
-
-    <!--  <div class="alert alert-success text-white d-flex align-items-center" role="alert"> -->
-
-        <svg class="bi flex-shrink-0 me-2" width="24" height="24" role="img" aria-label="Success:"><use xlink:href="#check-circle-fill"/></svg>
-        <div>
-          <?php echo $_SESSION['update_success_message']; ?>
- <button type="button" class="btn-close" data-bs-dismiss="alert"></button>
-        </div>
-      </div>
       <?php
-      unset($_SESSION['update_success_message']);
-    }
-    ?>
-
-    <!--delete alert -->
-    <?php if (isset($_SESSION['delete_success_message']) && !empty($_SESSION['delete_success_message'])) { ?>
-      <div class="alert alert-danger text-white alert-dismissible d-flex align-items-center fade show" role="alert">
-        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" class="bi bi-exclamation-triangle-fill flex-shrink-0 me-2" viewBox="0 0 16 16" role="img" aria-label="Warning:">
-  <path d="M8.982 1.566a1.13 1.13 0 0 0-1.96 0L.165 13.233c-.457.778.091 1.767.98 1.767h13.713c.889 0 1.438-.99.98-1.767L8.982 1.566zM8 5c.535 0 .954.462.9.995l-.35 3.507a.552.552 0 0 1-1.1 0L7.1 5.995A.905.905 0 0 1 8 5zm.002 6a1 1 0 1 1 0 2 1 1 0 0 1 0-2z"/>
-</svg>
-        <div>
-          <?php echo $_SESSION['delete_success_message']; ?>
-        </div>
-      </div>
-      <?php
-      unset($_SESSION['delete_success_message']);
-    }
-    ?>
-
+      messageAlert("success", "update_success_message");
+      messageAlert("danger", "delete_success_message"); ?>
     </div>
   </div>
 </div>
@@ -322,11 +259,8 @@ if(!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true){
 // get the element to animate
 var element = document.getElementById('count-stats');
 var elementHeight = element.clientHeight;
-
 // listen for scroll event and call animate function
-
 document.addEventListener('scroll', animate);
-
 // check if element is in view
 function inView() {
   // get window height
