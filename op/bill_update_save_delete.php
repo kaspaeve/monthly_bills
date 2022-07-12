a666245269a90b401d168d2860653e203c4a0cef
kaspaeve <koga.industries@gmail.com>
2022-07-11 22:17:41 -0500

    added delete if

--- op/bill_update_save_delete.php
+++ op/bill_update_save_delete.php
@@ -1,8 +1,7 @@
 <?php
 
 require_once '../config_save.php';
-  if(isset($_POST['updatebill'])){
-
+if(isset($_POST['updatebill'])){
   $bill_id = $_POST['bill_id'];
   $name = $_POST['bname'];
   $account = $_POST['accountnumber'];
@@ -14,38 +13,43 @@ require_once '../config_save.php';
 
   //inject to default_bills
   $sql2 = "UPDATE default_bills
-  SET bill_name = '$name', bill_due_date ='$bdate', bill_website ='$website', bill_account ='$account', bill_user ='$user',
-  bill_notes = '$notes'
+  SET bill_name = '$name', bill_due_date ='$bdate', bill_website ='$website', bill_account ='$account', bill_user ='$user', bill_notes = '$notes' WHERE id_bill = '$bill_id'";
+  mysqli_query($conn, $sql2);
+  //inject to history
+  $sql = "UPDATE history
+  SET bill_date = '$bdate', bill_name ='$name'
   WHERE id_bill = '$bill_id'";
 
-  mysqli_query($conn, $sql2);
-//inject to history
-$sql = "UPDATE history
-SET bill_date = '$bdate', bill_name ='$name'
-WHERE id_bill = '$bill_id'";
+  mysqli_query($conn, $sql);
 
-mysqli_query($conn, $sql);
+  session_start();
+  $_SESSION['update_success_message'] = "<strong>Success!</strong> $name has been <strong>updated</strong>.";
 
-session_start();
-$_SESSION['success_message'] = "Bill <strong>updated</strong>updated successfully.";
-header("Location: ../pages/bill_list.php");
-exit();
+  header("Location: ../pages/bill_list.php");
+  exit();
+  //  echo $bill_id."<br>";
+  //  echo $name."<br>";
+  //  echo $bill_id2."<br>";
+  //  echo $notes."<br>";
+  //  echo $bdate."<br>";
 
-
-    exit();
-  } elseif (isset($_POST['delete_bill'])){
+} elseif (isset($_POST['delete_bill'])){
   $bill_id = $_POST['bill_id'];
-  $sql2 = "DELETE FROM default_bills WHERE id_bill='$bill'";
+  $name = $_POST['bname'];
+  //  echo $bill_id."br";
+  //  echo $name."br";
+  $sql2 = "DELETE FROM default_bills WHERE id_bill='$bill_id'";
   mysqli_query($conn, $sql2);
 
-    session_start();
-    $_SESSION['success_message'] = "Bill <strong>deleted</strong> successfully.";
-    header("Location: ../pages/bill_list.php");
+  session_start();
+  $_SESSION['delete_success_message'] = "$name <strong>DELETED</strong> successfully.";
+  header("Location: ../pages/bill_list.php");
 
-  }
-  else{
+}
+else{
   echo "Failed to connect to database: " . mysqli_connect_error();
-  }
+  exit();
+}
 
 
 ?>
--- op/report_pay_delete_bill.php
+++ op/report_pay_delete_bill.php
@@ -2,61 +2,49 @@
 
 require_once '../config_save.php';
 if (isset($_POST['submit_bill'])) {
-
-
-$paid = $_POST['ubill_paid_amount'];
-$bnotes = $_POST['bnotes'];
-$obill = $_POST['obill_id'];
-$mbill = $_POST['mbill_id'];
-$year = $_POST['year1'];
-$month = $_POST['month1'];
-$today = date("Y-m-d H:i:s");
-
-//$account = mysqli_real_escape_string($conn, $_POST['accountnumber']);
-
-
-//echo $paid."<br>";
-//echo $bnotes."<br>";
-//echo $label."<br>";
-//echo $label2."<br>";
-//echo $year."<br>";
-//echo $month."<br>";
-//echo $today."<br>";
-$sql = "UPDATE history
-SET bill_paid_date = '$today', bill_paid_amount = '$$paid', bill_notes = '$bnotes'
-WHERE b_id = '$mbill' AND Year = '$year ' AND Month  ='$month'";
-
-mysqli_query($conn, $sql);
-
-$sql2 = "UPDATE default_bills
-SET bill_paid_date = '$today', bill_paid_amount = '$$paid', bill_notes = '$bnotes'
-WHERE id_bill = '$obill'";
-
-mysqli_query($conn, $sql2);
-
-
-    //Start the session if already not started.
-    session_start();
-    $_SESSION['success_message'] = "Bill <strong>updated</strong> successfully.";
-    header("Location: ../pages/bill_report.php?year=$year&month=$month");
+  $paid = $_POST['ubill_paid_amount'];
+  $bnotes = $_POST['bnotes'];
+  $obill = $_POST['obill_id'];
+  $mbill = $_POST['mbill_id'];
+  $year = $_POST['year1'];
+  $month = $_POST['month1'];
+  $today = date("Y-m-d H:i:s");
+  //$account = mysqli_real_escape_string($conn, $_POST['accountnumber']);
+  //echo $paid."<br>";
+  //echo $bnotes."<br>";
+  //echo $label."<br>";
+  //echo $label2."<br>";
+  //echo $year."<br>";
+  //echo $month."<br>";
+  //echo $today."<br>";
+  $sql = "UPDATE history
+  SET bill_paid_date = '$today', bill_paid_amount = '$$paid', bill_notes = '$bnotes'
+  WHERE b_id = '$mbill' AND Year = '$year ' AND Month  ='$month'";
+  mysqli_query($conn, $sql);
+  $sql2 = "UPDATE default_bills
+  SET bill_paid_date = '$today', bill_paid_amount = '$$paid', bill_notes = '$bnotes'
+  WHERE id_bill = '$obill'";
+  mysqli_query($conn, $sql2);
+  //Start the session if already not started.
+  session_start();
+  $_SESSION['success_message'] = "Bill <strong>updated</strong> successfully.";
+  header("Location: https://bills.theschellers.us/pages/bill_report.php?year=$year&month=$month");
   //print_r($conn);
-
-
-    exit();
+  exit();
 } elseif (isset($_POST['delete_bill'])){
-$mbill = $_POST['mbill_id'];
-$year = $_POST['year1'];
-$month = $_POST['month1'];
-$sql2 = "DELETE FROM history WHERE b_id='$mbill'";
-mysqli_query($conn, $sql2);
+  $mbill = $_POST['mbill_id'];
+  $year = $_POST['year1'];
+  $month = $_POST['month1'];
+  $sql2 = "DELETE FROM history WHERE b_id='$mbill'";
+  mysqli_query($conn, $sql2);
 
   session_start();
   $_SESSION['success_message'] = "Bill <strong>deleted</strong> successfully.";
-  header("Location: ..pages./bill_report.php?year=$year&month=$month");
+  header("Location: https://bills.theschellers.us/pages/bill_report.php?year=$year&month=$month");
 
 }
 else{
-echo "Failed to connect to database: " . mysqli_connect_error();
+  echo "Failed to connect to database: " . mysqli_connect_error();
 }
 
 ?>
